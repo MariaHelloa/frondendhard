8997a16aa60424b5fa6bea3411e28f5b0abf5859
MariaHelloa <115737038+MariaHelloa@users.noreply.github.com>
2023-03-24 09:23:38 -0300

    Delete pokemon.js

--- pokemon.js
+++
@@ -1,9 +0,0 @@
-const fetch = require ("node-fetch");
-
-fetch("https://pokeapi.co/api/v2/pokemon/ditto")
-    .then((result) => {
-        console.log(result);
-    })
-    .catch ((erro) => {
-        console.log(erro);
-    });
